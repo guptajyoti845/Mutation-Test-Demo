5183096e5daa14d3c0a11eeeaf73d1eed810e410
Jyoti Gupta <jyotiguptaofficial@gmail.com>
2021-08-17 19:33:33 +0530

    add test for mutation test coverage

--- src/test/java/org/vodqa/MatchmakerTest.java
+++ src/test/java/org/vodqa/MatchmakerTest.java
@@ -2,6 +2,7 @@ package org.vodqa;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.vodqa.Matchmaker.Gender.FEMALE;
 import static org.vodqa.Matchmaker.Gender.MALE;
@@ -19,4 +20,25 @@ public class MatchmakerTest {
     public void test19YearOldFemale() {
         assertTrue(agent.isEligibleForMarriage(19, FEMALE));
     }
+    @Test
+    public void testAllMales() {
+        assertFalse(agent.isEligibleForMarriage(17, MALE));
+        assertFalse(agent.isEligibleForMarriage(18, MALE));
+        assertFalse(agent.isEligibleForMarriage(19, MALE));
+        assertFalse(agent.isEligibleForMarriage(20, MALE));
+        assertTrue(agent.isEligibleForMarriage(21, MALE));
+        assertTrue(agent.isEligibleForMarriage(22, MALE));
+        assertFalse(agent.isEligibleForMarriage(17, FEMALE));
+        assertTrue(agent.isEligibleForMarriage(18, FEMALE));
+        assertTrue(agent.isEligibleForMarriage(19, FEMALE));
+        assertTrue(agent.isEligibleForMarriage(20, FEMALE));
+        assertTrue(agent.isEligibleForMarriage(21, FEMALE));
+        assertTrue(agent.isEligibleForMarriage(22, FEMALE));
+        assertFalse(agent.isEligibleForMarriage(17, null));
+        assertFalse(agent.isEligibleForMarriage(18, null));
+        assertFalse(agent.isEligibleForMarriage(20, null));
+        assertTrue(agent.isEligibleForMarriage(21, null));
+        assertTrue(agent.isEligibleForMarriage(22, null));
+    }
+
 }
